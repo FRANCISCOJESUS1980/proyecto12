186f29d0681b366e7feb5566f7d0321240020d93
FRANCISCOJESUS1980 <jesusgonzalezvergara@hotmail.com>
2025-01-20 20:38:19 +0100

    feat quitando comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,3 @@
-/*import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Favorites from './pages/Favorites/Favorites'
-import AddCharacter from './pages/AddCharacter/AddCharacter'
-import { FavoritesProvider } from './context/FavoritesContext'
-import './App.css'
-
-function App() {
-  return (
-    <FavoritesProvider>
-      <Router>
-        <nav>
-          <Link to='/'>Home</Link>
-          <Link to='/favorites'>Favorites</Link>
-          <Link to='/add-character'>Add Character</Link>
-        </nav>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/favorites' element={<Favorites />} />
-          <Route path='/add-character' element={<AddCharacter />} />
-        </Routes>
-      </Router>
-    </FavoritesProvider>
-  )
-}
-
-export default App*/
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Favorites from './pages/Favorites/Favorites'
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,3 @@
-/*import { Link } from 'react-router-dom'
-import './Header.css'
-
-const Header = () => {
-  return (
-    <header className='header'>
-      <img
-        src='/assets/imagenes/header.jpg'
-        alt='Dragon Ball Header'
-        className='header-image'
-      />
-      <nav className='nav'>
-        <Link to='/'>🏠 Inicio</Link>
-        <Link to='/favorites'>❤️ Favoritos</Link>
-        <Link to='/add-character'>➕ Agregar Personaje</Link>
-      </nav>
-    </header>
-  )
-}
-
-export default Header*/
 import { Link } from 'react-router-dom'
 import './Header.css'
 
@@ -26,7 +5,7 @@ const Header = () => {
   return (
     <header className='header'>
       <img
-        src='/assets/imagenes/header.jpg' // Asegúrate de que la imagen exista en la ruta correcta
+        src='/assets/imagenes/header.jpg'
         alt='Dragon Ball Header'
         className='header-image'
       />
